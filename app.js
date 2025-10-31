fec5c34160198d87ee8af5238889c43638bafd55
saini <107121488+erfaga@users.noreply.github.com>
2025-10-31 23:54:51 +0300

    Resolve null pointer exception in user service

--- app.js
+++ app.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-10-31T20:23:38.336Z
-Commit: Fix broken links in documentation
+Timestamp: 2025-10-31T20:54:52.976Z
+Commit: Resolve null pointer exception in user service
