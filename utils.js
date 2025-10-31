70212c0290a6be648b807be9268bbe7027979079
saini <107121488+erfaga@users.noreply.github.com>
2025-10-31 23:54:43 +0300

    Fix broken links in documentation

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-10-31T20:27:10.931Z
-Commit: Resolve timezone issues in date handling
+Timestamp: 2025-10-31T20:54:45.065Z
+Commit: Fix broken links in documentation
